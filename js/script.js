9f52a1812435857442ef79dd28c1c4da9c190235
Ashley Andrade <ashleyvitoriaandrade2004@gmail.com>
2026-06-26 19:23:44 -0300

    Adicionado o export e import do script_calculo.js para o script.js e adicionado ao script.js os dados para serem mostrados no html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// Fazendo os imports do script de cálculo
+import { Receber } from "./script_calculo.js"
+import { valorPremio } from "./script_calculo.js"
+
 //Pegando os dados do dom
 const formDados = document.querySelector('#formulario')
 const divResultado = document.querySelector('#resultado')
@@ -5,31 +9,38 @@ const divResultado = document.querySelector('#resultado')
 //Declarando o array
 const coletor = []
 
-formDados.addEventListener('submit', (evt)=>{
+formDados.addEventListener('submit', (evt) => {
     evt.preventDefault()
 
     const form_dados = new FormData(formDados)
 
     const coletor = {
-        descricao: (form_dados.get('descricao')),
-        quantidade: (form_dados.get('quantidade'))
-        
-        addColetor(coletor)
-        listColeor()
-
-        formDados.reset()
+        descricao: form_dados.get('descricao'),
+        quantidade: form_dados.get('quantidade')
     }
 
-    //Criando a função para adicionar os construtores
-    const addColetor = (objColetor) =>{
-        coletor.push(objColetor)
-    }
+    addColetor(coletor)
+    listColeor()
 
-    //Criando a função para que os coletores tenham suas descrições exibidas
-    const listColetor()=>{
-        coletor.forEach(elem,i => {
-            
-        });
-    }
-
+    formDados.reset()
 })
+
+//Criando a função para adicionar os construtores
+const addColetor = (objColetor) => {
+    coletor.push(objColetor)
+}
+
+//Criando a função para que os coletores tenham suas descrições exibidas
+const listColetor = () => {
+    coletor.forEach(elem => {
+        const recebido = Receber(elem)
+        const premio = valorPremio(elem)
+
+        divResultado.innerHTML +=
+            `A empresa ${elem.descricao} 
+            coletou ${elem.quantidade} 
+            de resíduos e o valor recebido foi de ${recebido} e ${premio}`
+
+    });
+}
+
--- js/script_calculo.js
+++ js/script_calculo.js
@@ -1,6 +1,7 @@
 const Receber = (objColetor) =>{
     const tonelada = objColetor.quantidade
     const valorRecebido = ''
+    const recebePremio = 'atingindo assim a meta esperada e portando recebendo a premiação de: R$'
 
     valorRecebido = tonelada * 220
 
@@ -11,15 +12,15 @@ const valorPremio = (objColetor) =>{
     const premio = ''
      
     if (valorRecebido < 1000){
-        premio = 'Não atingiu o valor '
+        premio = 'Não atingiu o valor para premiação'
     }else if (valorRecebido < 10000){
-        premio = 'R$' + (valorRecebido * 0.08) 
+        premio = recebePremio + (valorRecebido * 0.08) 
     }else if (valorRecebido < 15000){
-        premio = 'R$' + (valorRecebido * 0.10) 
+        premio = recebePremio + (valorRecebido * 0.10) 
     }else if (valorRecebido < 25000){
-        premio = 'R$' + (valorRecebido * 0.15) 
+        premio = recebePremio + (valorRecebido * 0.15) 
     }else {
-        premio = 'R$' + (valorRecebido * 0.20) 
+        premio = recebePremio + (valorRecebido * 0.20) 
     }
     
     return premio
@@ -27,5 +28,5 @@ const valorPremio = (objColetor) =>{
 
 const valorTotal = valorRecebido + premio
 
-export {valor,premio, valorTotal}
+export {Receber, valorPremio, valorTotal}
 
